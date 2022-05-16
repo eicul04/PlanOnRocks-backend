5e6454ad64a1ac67165053956adeee47eded4a46
lucie <weber_lucie@web.de>
2022-05-16 09:00:47 +0200

    categorize trip

--- 3-planOnRocks-domain/src/main/java/com/planOnRocks/domain/climbingRock/TripCategorizer.java
+++ 3-planOnRocks-domain/src/main/java/com/planOnRocks/domain/climbingRock/TripCategorizer.java
@@ -1,16 +1,34 @@
 package com.planOnRocks.domain.climbingRock;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class TripCategorizer {
 
-    private DistanceCalculator destinationCalculator;
+    @Autowired
+    public TripCategorizer(DistanceCalculator distanceCalculator) {
+        this.distanceCalculator = distanceCalculator;
+    }
+
+    private DistanceCalculator distanceCalculator;
 
     public TripCategory categorizeClimbingRock(ClimbingRock climbingRock, Location userLocation) {
         Location climbingRockLocation = climbingRock.getClimbingRockLocation();
-        Distance destination = destinationCalculator.getDistance(climbingRockLocation, userLocation);
-        // TODO Logik implementieren, grade noch Mock
-        if(destination.getValue() > 5) {
+        Distance distance = distanceCalculator.getDistance(climbingRockLocation, userLocation);
+        return getTripCategory(distance);
+    }
+
+    private TripCategory getTripCategory(Distance distance) {
+        if(distance.getValue() < 50) {
+            return TripCategory.HALF_DAY_TRIP;
+        }
+        if(distance.getValue() < 140) {
             return TripCategory.DAY_TRIP;
         }
-        return TripCategory.HALF_DAY_TRIP;
+        if(distance.getValue() < 250) {
+            return TripCategory.WEEKEND_TRIP;
+        }
+        return TripCategory.VACATION;
     }
 }
