0ebf4c7ec5554ca01e7807f6ea27b22160967b4b
lucie <weber_lucie@web.de>
2022-05-17 00:58:53 +0200

    add test cases

--- 2-planOnRocks-application/src/test/java/com/planOnRocks/application/ClimbingRockFilterServiceTest.java
+++ 2-planOnRocks-application/src/test/java/com/planOnRocks/application/ClimbingRockFilterServiceTest.java
@@ -1,7 +1,6 @@
 package com.planOnRocks.application;
 
 import com.planOnRocks.application.climbingRock.ClimbingRockFilterService;
-import com.planOnRocks.application.climbingRock.ClimbingRockService;
 import com.planOnRocks.application.climbingRock.TripCategorizerService;
 import com.planOnRocks.domain.climbingRock.ClimbingRock;
 import com.planOnRocks.domain.climbingRock.ClimbingRockRepository;
@@ -9,7 +8,9 @@ import com.planOnRocks.domain.climbingRock.enums.Bolting;
 import com.planOnRocks.domain.climbingRock.enums.Difficulty;
 import com.planOnRocks.domain.climbingRock.enums.TripCategory;
 import com.planOnRocks.domain.climbingRock.valueObjects.Location;
+import com.planOnRocks.domain.trip.Trip;
 import com.planOnRocks.domain.trip.enums.ParticipantExperience;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -42,19 +43,47 @@ public class ClimbingRockFilterServiceTest {
     private static final Location USER_LOCATION = new Location(LATITUDE_2, LONGITUDE_2);
     private static final TripCategory TRIP_CATEGORY = TripCategory.DAY_TRIP;
     private static final String NAME = "Gausbach";
-    private static final ParticipantExperience PARTICIPANT_EXPERIENCE = ParticipantExperience.ADVANCED;
+    private static final ParticipantExperience PARTICIPANT_EXPERIENCE_BEGINNER = ParticipantExperience.BEGINNER;
+    private static final ParticipantExperience PARTICIPANT_EXPERIENCE_ADVANCED = ParticipantExperience.ADVANCED;
+    private static final ParticipantExperience PARTICIPANT_EXPERIENCE_PROFESSIONAL = ParticipantExperience.PROFESSIONAL;
+
+    private List<ClimbingRock> climbingRockListMock;
+    private ClimbingRock climbingRockMock;
+
+    @BeforeEach
+    void init() {
+        this.climbingRockMock = new ClimbingRock(CLIMBING_ROCK_LOCATION, NAME, Difficulty.EASY, Bolting.VERY_GOOD);
+        this.climbingRockListMock = new ArrayList<>();
+        climbingRockListMock.add(climbingRockMock);
+    }
 
     @Test
-    void getClimbingRocksWithTripConditions() {
-        ClimbingRock climbingRockMock = new ClimbingRock(CLIMBING_ROCK_LOCATION, NAME, Difficulty.EASY, Bolting.VERY_GOOD);
-        List<ClimbingRock> climbingRockListMock = new ArrayList<>();
-        climbingRockListMock.add(climbingRockMock);
+    void getClimbingRocksWithParticipantExperienceBeginner() {
+        doReturn(climbingRockListMock).when(climbingRockRepository).getClimbingRocksForBeginnerParticipants();
+        doReturn(TRIP_CATEGORY).when(tripCategorizerService).getTripCategory(climbingRockMock, USER_LOCATION);
 
+        List<ClimbingRock> resultList =
+                climbingRockFilterService.getClimbingRocksWithTripConditions(PARTICIPANT_EXPERIENCE_BEGINNER, TRIP_CATEGORY, USER_LOCATION);
+        assertEquals(climbingRockListMock, resultList);
+    }
+
+    @Test
+    void getClimbingRocksWithParticipantExperienceAdvanced() {
         doReturn(climbingRockListMock).when(climbingRockRepository).getClimbingRocksForAdvancedParticipants();
         doReturn(TRIP_CATEGORY).when(tripCategorizerService).getTripCategory(climbingRockMock, USER_LOCATION);
 
         List<ClimbingRock> resultList =
-                climbingRockFilterService.getClimbingRocksWithTripConditions(PARTICIPANT_EXPERIENCE, TRIP_CATEGORY, USER_LOCATION);
+                climbingRockFilterService.getClimbingRocksWithTripConditions(PARTICIPANT_EXPERIENCE_ADVANCED, TRIP_CATEGORY, USER_LOCATION);
+        assertEquals(climbingRockListMock, resultList);
+    }
+
+    @Test
+    void getClimbingRocksWithParticipantExperienceProfessional() {
+        doReturn(climbingRockListMock).when(climbingRockRepository).getClimbingRocksForProfessionalParticipants();
+        doReturn(TRIP_CATEGORY).when(tripCategorizerService).getTripCategory(climbingRockMock, USER_LOCATION);
+
+        List<ClimbingRock> resultList =
+                climbingRockFilterService.getClimbingRocksWithTripConditions(PARTICIPANT_EXPERIENCE_PROFESSIONAL, TRIP_CATEGORY, USER_LOCATION);
         assertEquals(climbingRockListMock, resultList);
     }
 }
